bfc66360f6040c860846908c04b93f8a4e181ca0
ayomibaby <liasutolulope@gmail.com>
2023-09-06 07:39:39 +0100

    top Artists component

--- app/page.js
+++ app/page.js
@@ -1,13 +1,20 @@
-import List from '@/components/List'
-import Image from 'next/image'
+import List from "@/components/List";
+import TopArtists from "@/components/TopArtists/TopArtists";
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen  flex-col items-center font-white bg-[#090A0C]">
-      <div className='h-46 text-white mt-20 '>
-jhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh
+    <main className=" min-h-screen bg-white  md:bg-[#090A0C]">
+      <div className="w-[90%] mx-auto pt-[2rem]">
+        <TopArtists/>
       </div>
-      <List/>
     </main>
-  )
+  );
 }
+
+{
+  /* <div className='h-46 text-white mt-20 '>
+jhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh 
+      </div> */
+}
+// <List/>
---
+++ components/TopArtists/TopArtists.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+//svgs
+import MoreCircle from "../../public/assets/svg/moreCircle.svg"
+
+export default function TopArtists() {
+  return (
+    <section id='topArtists'>
+        <section id='heading'>
+            <div>
+                <div className='flex gap-x-2'>
+                <h2>Top Artists</h2>
+                <img src={MoreCircle}/>
+                </div>
+                <h5 className='mt-[0.5rem] flex gap-x-1'>You listened to <h5 className='text-[#306EF9]'>17 Artists</h5> this past week</h5>
+            </div>
+            <div>
+                
+            </div>
+        </section>
+    </section>
+  )
+}
