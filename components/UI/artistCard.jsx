fce3ff090bdbd2474408c3976e6828e0e5fe6058
ayomibaby <liasutolulope@gmail.com>
2023-09-09 13:34:36 +0100

    crd start

--- components/UI/artistCard.jsx
+++ components/UI/artistCard.jsx
@@ -1,10 +1,18 @@
 import { Beyonce } from '@/public/assets/svg/moreCircle'
+import Image from 'next/image'
 import React from 'react'
 
 export default function ArtistCard() {
   return (
  <section>
     <div>{Beyonce}</div>
+    <Image
+      src={"https://res.cloudinary.com/dhvwthnzq/image/upload/v1694000577/spotify/Resized_pvxs9n.jpg"}
+      width={50}
+      height={50}
+      alt='shoes'
+    />
+
  </section>
   )
 }
