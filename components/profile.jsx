924abe7446a6b464d0158be0a8f1cf3ce0743975
Ayodeji Atanda <ayodejiatanda1@gmail.com>
2024-01-04 00:17:27 +0100

    feat: getting user followers

--- components/profile.jsx
+++ components/profile.jsx
@@ -1,10 +1,29 @@
 "use client"
+import useSpotify from "@/hooks/useSpotify";
 import { useSession } from "next-auth/react";
 import Image from "next/image";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 export default function Profile() {
+  const spotifyApi = useSpotify()
   const { data: session} = useSession()
+  const [followers, setFollowers] = useState([]);
+
+  useEffect(() => {
+    console.log(session)
+    if (spotifyApi.getAccessToken) {
+      spotifyApi
+        .getMe()
+        .then((data) => {
+           setFollowers(data.body.followers.total)
+          console.log(data.body)
+        })
+        .catch(err =>{
+          return err
+        });
+    }
+  }, [session, spotifyApi]);
+
   return (
     <section className="w-[100%]  mt-5 bg-[#121213]">
       <div className="w-[90%] md:w-[95%] mx-auto py-[2.5rem] flex justify-center ">
@@ -20,7 +39,7 @@ export default function Profile() {
         </div>
         <div>
           <h2>{session?.user.name}</h2>
-          <h5 className="text-[#BFBFBF]">1 follower</h5>
+          <h5 className="text-[#BFBFBF]">{followers} follower</h5>
         </div>
         </div>
       </div>
